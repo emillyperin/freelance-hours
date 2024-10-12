2ce0cd60a64bd19a5fe1ee7f352214f7ca3a472c
Emily <emily.perin@webjump.com.br>
2024-10-12 15:15:40 -0300

    saving proposals

---
+++ app/Livewire/Proposals/Create.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Livewire\Proposals;
+
+use App\Models\Project;
+use Livewire\Component;
+use Illuminate\Validation\Rule;
+
+class Create extends Component
+{
+    public Project $project;
+    public bool $modal = true;
+    public string $email = '';
+    public int $hours = 0;
+
+    protected $rules = [
+        'email' => ['required', 'email'],
+        'hours' => ['required', 'numeric', 'gt:0'],
+    ];
+
+    public bool $agree = false;
+
+    public function save()
+    {
+        
+        $this->validate();
+        
+        if (! $this->agree) {
+            $this->addError('agree', 'Você precisa aceitar os termos e condicoes');
+            return;
+        }
+
+        $this->project->proposals()
+            ->updateOrCreate([
+                'email' => $this->email,
+            ], [
+                'hours' => $this->hours,
+            ]);
+        
+        $this->modal = false;
+    }
+
+    public function render()
+    {
+        return view('livewire.proposals.create');
+    }
+}
